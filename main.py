696562c096b0beee791c906b2d925b8936690b65
VeGnError <arsreplay@rambler.ru>
2023-10-23 19:14:41 +0300

    second commit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from typing import List
-
-from fastapi import FastAPI
+from typing import List, Optional
+from fastapi.exceptions import HTTPException
+from fastapi import FastAPI, status
 from pydantic import BaseModel, Field
+from numpy import size
 
 app = FastAPI(
     title="Music"
@@ -22,29 +23,38 @@ music_add = [
 
 
 class User(BaseModel):
-    id: int
-    role: str
-    Name: str
+    id: Optional[int]
+    role: Optional[str]
+    Name: Optional[str]
 
 
-@app.get("/users/{user_id}", response_model=List[User])
-def get_user(user_id: int):
+@app.get("/users/{user_id}", status_code=status.HTTP_200_OK, response_model=List[User])
+def get_user(user_id: int = 1):
+    if 0 >= user_id or user_id > size(users):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Wrong id")
+
     return [user for user in users if user.get("id") == user_id]
 
 
-@app.get("/Janrs")
-def get_janrs(kol_vo_janrs: int = 1, offset: int = 0):
-    return music_add[offset:][:kol_vo_janrs]
+@app.get("/Music_list", status_code=status.HTTP_200_OK)
+def get_song(music_list_id: int = 1, offset: int = 0):
+    if 0 >= music_list_id or music_list_id > size(music_add):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Wrong id")
+
+    return music_add[offset:][:music_list_id]
 
 
-@app.post("/users")
+@app.post("/users", status_code=status.HTTP_200_OK)
 def post_user(user: str):
     users3.append(user)
     return users3
 
 
-@app.post("/users/{user_id}")
-def change_user_role(user_id: int, new_role: str):
+@app.put("/users/{user_id}", status_code=status.HTTP_200_OK)
+def change_user_role(user_id: int = 1, new_role: str = "Null"):
+    if 0 >= user_id or user_id > size(users):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Wrong id")
+
     current_user = list(filter(lambda user: user.get("id") == user_id, users))[0]
     current_user["role"] = new_role
     return{"status": 200, "data": current_user}
@@ -57,17 +67,15 @@ fake_trades = [
 
 
 class Trade(BaseModel):
-    id: int
-    user_id: int
-    currency: str = Field(max_length=5)
-    side: str
-    price: float = Field(ge=0)
-    amount: float
+    id: Optional[int]
+    user_id: Optional[int]
+    currency: Optional[str] = Field(max_length=5)
+    side: Optional[str]
+    price: Optional[float] = Field(ge=0)
+    amount: Optional[float]
 
 
-@app.post("/trades")
+@app.post("/trades", status_code=status.HTTP_200_OK)
 def add_trades(trades: List[Trade]):
     fake_trades.extend(trades)
     return {"status": 200, "data": fake_trades}
-
-
